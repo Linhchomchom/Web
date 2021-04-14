2707d5d5729b87492188fc5460155dee22d614c5
Linhchomchom <82081662+Linhchomchom@users.noreply.github.com>
2021-04-14 10:54:14 +0700

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 $serverName = "localhost";
 $port = 3306;
-$username = "";
-$password = "";
+$username = "Sydney St James";
+$password = "e61d6c9a5685a12bcefdefb0606d6ef5ba51b147068ba38f3d02947536e14924";
 $databaseName = "toystore";
 ?>
